2a9807e732ffb7ca327a7ed80fd63f2a95adbaa2
Joseph Bateman <josephbateman@Josephs-MacBook-Pro-2.local>
2025-02-24 14:40:18 +0000

    linting error fixed in about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,11 +23,11 @@ const About: React.FC = () => {
                     Led by Joe Bateman, a talented multi-instrumentalist, singer-songwriter, and producer, the band provides top-tier live performances that guarantee a packed dance floor at every event.
                   </p>
                   <p>
-                    Whether you're organizing a wedding, corporate event, birthday party, or private function, The JB Experience brings the perfect energy and atmosphere to your celebration.<br /><br />
+                    Whether you&apos;re organising a wedding, corporate event, birthday party, or private function, The JB Experience brings the perfect energy and atmosphere to your celebration.<br /><br />
                     Their seamless blend of music and personalized performances makes them the ideal choice for events in East London and beyond.
                   </p>
                   <p>
-                    Joe Bateman’s extensive experience includes performances at the prestigious Isle of Wight Festival, features on BBC Radio London, and millions of Spotify streams.<br /><br />
+                    Joe Bateman&apos;s extensive experience includes performances at the prestigious Isle of Wight Festival, features on BBC Radio London, and millions of Spotify streams.<br /><br />
                     His passion for music and his ability to connect with audiences makes every show a one-of-a-kind experience.
                   </p>
                   <p>
@@ -35,7 +35,7 @@ const About: React.FC = () => {
                     Their versatility, professionalism, and reputation for excellence make them the go-to choice for any celebration.
                   </p>
                   <p>
-                    If you're looking for exceptional live entertainment for your event, book The JB Experience today for a night of unforgettable music that your guests will talk about for years to come.
+                    If you&apos;re looking for exceptional live entertainment for your event, book The JB Experience today for a night of unforgettable music that your guests will talk about for years to come.
                   </p>
                 </div>
               )}
